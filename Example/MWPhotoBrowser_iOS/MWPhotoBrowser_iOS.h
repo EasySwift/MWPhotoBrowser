4f09c8c44e8ee5939ed4a09626ea12f36fb6897e
stubbornnessness <1219721814yxj>
2016-10-12 01:45:52 +0800

    2.2版本开始支持carthage

---
+++ Example/MWPhotoBrowser_iOS/MWPhotoBrowser_iOS.h
@@ -0,0 +1,19 @@
+//
+//  MWPhotoBrowser_iOS.h
+//  MWPhotoBrowser_iOS
+//
+//  Created by yuanxiaojun on 2016/10/12.
+//  Copyright © 2016年 Michael Waterfall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MWPhotoBrowser_iOS.
+FOUNDATION_EXPORT double MWPhotoBrowser_iOSVersionNumber;
+
+//! Project version string for MWPhotoBrowser_iOS.
+FOUNDATION_EXPORT const unsigned char MWPhotoBrowser_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MWPhotoBrowser_iOS/PublicHeader.h>
+
+
